e0a64d97b11a3ab78cc8eeee2594866f33213e6f
panjigading <panjigading@student.ub.ac.id>
2025-11-04 19:53:32 +0700

    Perbaiki rute homepage dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('homepage');
+});
+
+Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
