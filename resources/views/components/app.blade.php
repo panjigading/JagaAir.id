8fee50c9a980770a209165cc7d80f2cc511fa49c
iqbalfrn <iqbaalllfernan@gmail.com>
2025-11-08 20:41:35 +0700

    tambahan styling untuk halaman riwayat update laporan dan beberapa penyesuaian pada homepage

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JagaAir.ID</title>
 
-    <!-- saya meletakkan css di sini -->
     <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
     
     @yield('css') <!-- untuk CSS tambahan per halaman -->
@@ -13,7 +12,6 @@
 <body>
     {{ $slot }}
     
-    <!-- js diletakkan di sini -->
     <script src="{{ asset('js/homepage.js') }}"></script>
     @yield('js') <!-- untuk js tambahan per halaman -->
 </body>
