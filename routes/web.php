5942d308c105230de775634276376e7ef46bd37a
panjigading <panjigading@student.ub.ac.id>
2025-11-09 16:05:20 +0700

    Merapikan dan menamakan link di web.php

--- routes/web.php
+++ routes/web.php
@@ -6,25 +6,25 @@ use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('homepage');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->name('admin.dashboard');
 
 Route::get('/dashboard/detail_laporan', function () {
     return view('detail_laporan');
-});
+})->name('admin.detail_laporan');
 
-Route::get('/admin/daftar_laporan', function () {
+Route::get('/dashboard/daftar_laporan', function () {
     return view('admin.daftar_laporan');
-});
+})->name('admin.daftar_laporan');
 
-Route::get('/kotak_saran', function () {
+Route::get('/dashboard/kotak_saran', function () {
     return view('kotak_saran');
-});
+})->name('admin.kotak_saran');
 
-Route::get('/kotak_saran/{id}', function ($id) {
+Route::get('/dashboard/kotak_saran/{id}', function ($id) {
     $suggestions = [
         '00001' => ['id' => '00001', 'name' => 'ANONIM', 'date' => '20 MAR 2025', 'category' => 'Infrastruktur Air', 'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam leo sapien, ultricies id quam sit amet, feugiat laoreet magna.'],
         '00002' => ['id' => '00002', 'name' => 'ANONIM', 'date' => '28 May 2025', 'category' => 'Infrastruktur Air', 'content' => 'Contoh saran lainnya...'],
@@ -44,23 +44,23 @@ Route::get('/kotak_saran/{id}', function ($id) {
     }
 
     return view('detail_saran', ['suggestion' => $suggestion]);
-})->where('id', '[0-9]+')->name('detail.saran'); 
+})->where('id', '[0-9]+')->name('admin.detail_saran'); 
 
-Route::get('/saran', function () {
+Route::get('/form_saran', function () {
     return view('form_saran');
-});
+})->name('form_saran');
 
 Route::get('/sign_up', function () {
     return view('sign_up');
-});
+})->name('sign_up');
 
 Route::get('/sign_in', function () {
     return view('sign_in');
-});
+})->name('sign_in');
 
 Route::get('/cari_laporan', function () {
     return view('cari_laporan');
-});
+})->name('cari_laporan');
 
 Route::post('/logout', function (Request $request) {
     if (function_exists('auth') && auth()->check()) {
@@ -71,7 +71,7 @@ Route::post('/logout', function (Request $request) {
     return redirect('/');
 })->name('logout');
 
-Route::get('/pencarian_laporan/{id}', function ($id) {
+Route::get('/cari_laporan/{id}', function ($id) {
     $suggestions = [
         '00001' => ['id' => '00001', 'name' => 'ANONIM', 'date' => '20 MAR 2025', 'category' => 'Infrastruktur Air', 'location' => 'Lowokwaru, Jatimulyo'],
     ];
@@ -83,4 +83,4 @@ Route::get('/pencarian_laporan/{id}', function ($id) {
     }
 
     return view('riwayat_update_laporan', compact('report'));
-})->name('pencarian_laporan');
+})->name('riwayat_update_laporan');
