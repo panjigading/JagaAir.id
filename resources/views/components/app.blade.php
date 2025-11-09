0606ef2e07deed051c536410c2448b9e442fa5fc
panjigading <panjigading@student.ub.ac.id>
2025-11-09 16:34:25 +0700

    Perbaiki font homepage

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -6,6 +6,9 @@
     <title>JagaAir.ID</title>
 
     <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital,wght@0,400;0,700;1,400;1,700&family=Pontano+Sans&display=swap" rel="stylesheet">
     
     @yield('css') <!-- untuk CSS tambahan per halaman -->
 </head>
