17af073ecb37f798fd3ea24c3105017dd0a8c592
panjigading <panjigading@student.ub.ac.id>
2025-11-13 13:56:46 +0700

    Tambah migration tabel laporan

---
+++ database/migrations/2025_11_13_064723_create_laporan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laporan', function (Blueprint $table) {
+            $table->id();
+            $table->string('alamat_lengkap')->nullable();
+            $table->string('kecamatan');
+            $table->string('kelurahan');
+            $table->string('kategori');
+            $table->text('detail');
+            $table->string('url_bukti');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laporan');
+    }
+};
